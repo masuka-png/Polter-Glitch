6c7aa0d4820daa61b17cf659cc4697f2d7d45297
Sam Enright <same23710@gmail.com>
2026-04-25 00:29:10 +0100

    Added Enemy Ai (work in progress)

---
+++ Assets/AIController.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using System.Collections;
+
+
+public enum EnemyState
+{
+    Patrolling,
+    Following
+}
+
+
+public class AIController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform[] patrolPoints;
+
+    [SerializeField] private float patrolWaitTime = 2f;
+    [SerializeField] private float stopAtDistance = 0.5f;
+    [SerializeField] private float detectionRange = 5f;
+    [SerializeField] private float viewAngle = 90f;
+    [SerializeField] private flaot losePlayerTime = 3f;
+
+    private UnityEngine.AI.NavMeshAgent _agent;
+    private Animator _animator;
+    private int _currentPatrolIndex;
+    private bool _isWaiting;
+    private flaot _timeSincePlayerLost;
+  
+
+
+    private void Awake()
+    {
+        _agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        var distanceToPlayer = Vector3.Distance(player.position, transform.position)
+
+        switch (_state)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+                if (distanceToPlayer <= detectionRange && CanSeePlayer())
+                {
+                    _state = EnemyState.Following;
+                }
+
+                break;
+
+            case EnemyState.Following:
+                FollowPlayer();
+                if (!CanSeePlayer())
+                {
+                    _timeSincePlayerLost += losePlayerTime.deltaTime;
+                    if (_timeSincePlayerLost >= losePlayerTime)
+                    {
+                        _state = EnemyState.Patrolling;
+                        GotoClosestPatrolPoint();
+                    }
+                }
+        }
+
+        UpdateAnimations();
+    }
+
+    private void FollowPlayer
+    {
+        _agent.SetDestination(player.position);
+    }
+
+
+
+    private void Patrol()
+    {
+        if (_isWaiting) return;
+        if (!_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            StartCoroutine(WaitAtPatrolPoint());
+        }
+    }
+
+     private IEnumerator WaitAtPatrolPoint()
+    {
+        _isWaiting = true;
+        _agent.isStopped = true;
+
+        yield return new WaitForSeconds(patrolWaitTime);
+
+        _agent.isStopped = false;
+        GoToNextPatrolPoint();
+        _isWaiting = false;
+    }
+
+
+
+
+
+    private void GoToNextPatrolPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+
+        _agent.SetDestination(patrolPoints[_currentPatrolIndex].position);
+        _currentPatrolIndex = (_currentPatrolIndex + 1) % patrolPoints.Length;
+    }
+
+    private void UpdateAnimations()
+    {
+        bool isMoving =
+            _agent.hasPath &&
+            !_agent.pathPending &&
+            _agent.remainingDistance > _agent.stoppingDistance &&
+            _agent.velocity.magnitude > 0.1f;
+
+        _animator.SetBool(IsWalking, isMoving);
+    }
+
+    private bool CanSeePlayer()
+    {
+        return IsFacingPlayer() && HasClearPathToPlayer();
+    }
+
+    private bool IsFacingPlayer()
+    {
+        var dirToPlayer = (player.position - transform.position).normalized;
+        var angle = Vector3.Angle(transform.forward,dirToPlayer);
+        return angle <= viewAngle / 2f;
+    }
+
+    private bool HasClearPathToPlayer()
+    {
+        var dirToPlayer = player.position - transform.position;
+        if (Physics.Raycast(transform.position, dirToPlayer.normalized, out RaycastHit hit, dirToPlayer.magnitude))
+        {
+            return hit.transform == player;
+        }
+
+        return true;
+    }
+
+    private void GoToClosestPatrolPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+        var closestIndex = 0;
+        var closestDistance = float.MaxValue;
+        
+ 
+    }
+}
