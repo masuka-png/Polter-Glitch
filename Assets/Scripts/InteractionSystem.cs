80cadc5091937c0e08c5675cb20e8e8b7000007b
Sam Enright <same23710@gmail.com>
2026-05-03 18:26:45 +0100

    Interaction System (WIP)

---
+++ Assets/Scripts/InteractionSystem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+public class InteractionSystem : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius = 0.05f;
+    [SerializeField] private LayerMask _interactableMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private int _numFound;
+
+    private void Update()
+    {
+        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+}
